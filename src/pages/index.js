8f128820e99617c406ececc904c37134a26aa731
Isaac Delgado <isaacdelgado1994@gmail.com>
2023-12-02 10:31:24 -0800

    utilizing built in css modules

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,22 @@
 import * as React from "react";
 import Layout from "../components/Layout";
+import { page, text } from "../examples/home.module.css";
 
 export default function Home() {
   return (
     <Layout>
-      <h1>Home Page</h1>
-      <a href="https://www.google.com">google</a>
+      <div className={page}>
+        <h1>Home Page</h1>
+        <p>
+          Minim officia ipsum veniam sit fugiat consequat pariatur eiusmod
+          laboris. Commodo occaecat velit mollit est mollit ad. Enim id
+          exercitation sint deserunt anim deserunt ipsum duis nulla labore
+          consequat elit minim. Eiusmod aute adipisicing aliquip laboris.
+          Laborum nisi eiusmod consectetur aliqua commodo. Magna officia veniam
+          non est reprehenderit tempor. Officia reprehenderit dolore dolor enim
+          cillum pariatur aliquip officia nulla sunt.
+        </p>
+      </div>
     </Layout>
   );
 }
