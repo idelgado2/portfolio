46cbf1fab1aefe566fa2d2a255964d8332785ebf
Isaac Delgado <isaacdelgado1994@gmail.com>
2023-12-04 20:23:06 -0800

    adding in footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./Navbar.js";
 import Footer from "./Footer.js";
+import "normalize.css";
 import "../assets/css/main.css";
 
 export default function Layout({ children }) {
