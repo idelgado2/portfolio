fb48d5711473d04fd81ff73a69ab265e408b9886
Isaac Delgado <isaacdelgado1994@gmail.com>
2023-12-07 20:36:29 -0800

    adding in react-icons and justify icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,28 @@
 import React from "react";
 import { Link } from "gatsby";
+import { FaAlignJustify } from "react-icons/fa6";
 
 export default function Navbar() {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">home</Link>
-          </li>
-          <li>
-            <Link to="/about">about</Link>
-          </li>
-          <li>
-            <Link to="/contact">contact</Link>
-          </li>
-          <li>
-            <Link to="/tags">tags</Link>
-          </li>
-          <li>
-            <Link to="/recipes">recipes</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <nav>
+      <FaAlignJustify />
+      <ul>
+        <li>
+          <Link to="/">home</Link>
+        </li>
+        <li>
+          <Link to="/about">about</Link>
+        </li>
+        <li>
+          <Link to="/contact">contact</Link>
+        </li>
+        <li>
+          <Link to="/tags">tags</Link>
+        </li>
+        <li>
+          <Link to="/recipes">recipes</Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
