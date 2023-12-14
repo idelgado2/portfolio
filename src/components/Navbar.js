f35db7e1f9586f4f9a2c4a16f9c091ea56fc3a0c
Isaac Delgado <isaacdelgado1994@gmail.com>
2023-12-14 10:46:07 -0800

    checking that this works in VS code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,6 +14,7 @@ export default function Navbar() {
     }
   }
   return (
+    // okay now this is verified
     <nav className="navbar">
       <div className="nav-center">
         <div className="nav-header">
