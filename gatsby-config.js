45b091e1301a73581ebdcca98b658e500d1ec9f8
Isaac Delgado <isaacdelgado1994@gmail.com>
2025-01-03 15:02:11 -0800

    removing image path reference

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,13 +25,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "content",
